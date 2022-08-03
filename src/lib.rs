959c1173831d48040c5787eb151fe7ca6fe92b1f
Piturnah <peterhebden6@gmail.com>
2022-08-03 14:31:46 +0100

    Move FASM-x86_64 header
    We are targeting multiple platforms now so it doesn't make as much sense
    to put this at the top, I think.

--- src/lib.rs
+++ src/lib.rs
@@ -4,44 +4,6 @@ use std::{
     process,
 };
 
-const ASM_HEADER: &str = "format ELF64 executable 3
-entry main
-segment readable executable
-print:
-  mov     r9, -3689348814741910323
-  sub     rsp, 40
-  mov     BYTE [rsp+31], 10
-  lea     rcx, [rsp+30]
-.L2:
-  mov     rax, rdi
-  lea     r8, [rsp+32]
-  mul     r9
-  mov     rax, rdi
-  sub     r8, rcx
-  shr     rdx, 3
-  lea     rsi, [rdx+rdx*4]
-  add     rsi, rsi
-  sub     rax, rsi
-  add     eax, 48
-  mov     BYTE [rcx], al
-  mov     rax, rdi
-  mov     rdi, rdx
-  mov     rdx, rcx
-  sub     rcx, 1
-  cmp     rax, 9
-  ja      .L2
-  lea     rax, [rsp+32]
-  mov     edi, 1
-  sub     rdx, rax
-  xor     eax, eax
-  lea     rsi, [rsp+32+rdx]
-  mov     rdx, r8
-  mov     rax, 1
-  syscall
-  add     rsp, 40
-  ret
-main:\n";
-
 pub struct Token<'l> {
     op: Op,
     loc: Loc<'l>,
@@ -337,7 +299,45 @@ impl<'a> Program<'a> {
     pub fn generate_fasm_x86_64_linux(self) -> String {
         let program = self.program;
 
-        let mut outbuf = String::from(ASM_HEADER);
+        let mut outbuf = String::from(
+            "format ELF64 executable 3
+entry main
+segment readable executable
+print:
+  mov     r9, -3689348814741910323
+  sub     rsp, 40
+  mov     BYTE [rsp+31], 10
+  lea     rcx, [rsp+30]
+.L2:
+  mov     rax, rdi
+  lea     r8, [rsp+32]
+  mul     r9
+  mov     rax, rdi
+  sub     r8, rcx
+  shr     rdx, 3
+  lea     rsi, [rdx+rdx*4]
+  add     rsi, rsi
+  sub     rax, rsi
+  add     eax, 48
+  mov     BYTE [rcx], al
+  mov     rax, rdi
+  mov     rdi, rdx
+  mov     rdx, rcx
+  sub     rcx, 1
+  cmp     rax, 9
+  ja      .L2
+  lea     rax, [rsp+32]
+  mov     edi, 1
+  sub     rdx, rax
+  xor     eax, eax
+  lea     rsi, [rsp+32+rdx]
+  mov     rdx, r8
+  mov     rax, 1
+  syscall
+  add     rsp, 40
+  ret
+main:\n",
+        );
 
         let mut jump_target_count = 0;
 
